a258ab0393120113c7c79dcf12edd3d346f91e83
Hugo Haas <hugoh@hugoh.net>
2024-07-14 09:41:16 -0500

    Fixed DA cancel

--- internal/down-action_test.go
+++ internal/down-action_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -28,4 +29,19 @@ func Test_ExecuteNonExistent(t *testing.T) {
 	}
 	err := da.Execute()
 	assert.Error(t, err)
+}
+
+func Test_StartAndStop(t *testing.T) {
+	every := time.Second
+	da := &DownAction{
+		After: 1 * time.Millisecond,
+		Every: every,
+		Exec:  "true",
+	}
+	da.Start()
+	time.Sleep(every) // Give it time to start
+	assert.Equal(t, true, da.isRunning(), "DownAction loop is running")
+	da.Stop()
+	time.Sleep(every) // Give it time to stop
+	assert.Equal(t, false, da.isRunning(), "DownAction loop is not running anymore")
 }
