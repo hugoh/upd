f3e2fb0ad1c0b0d1662907e42631b742f65b1809
Hugo Haas <hugoh@hugoh.net>
2024-11-21 22:57:08 -0600

    Re-added copyright statement that got lost

--- pkg/probe.go
+++ pkg/probe.go
@@ -1,3 +1,7 @@
+// Initially from: https://github.com/jesusprubio/up @ 784898b4b4e72ccb80b520c0dfbe8ebbc72b87fe
+// Copyright Jesús Rubio <jesusprubio@gmail.com>
+// MIT License
+
 package pkg
 
 import (
